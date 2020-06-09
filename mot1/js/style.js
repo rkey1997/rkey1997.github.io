db32cb98f34695c0716c898614400c733832e713
rkey1997 <kopytov_rodion@mail.ru>
2020-06-09 16:39:15 +0400

    42342

--- mot1/js/style.js
+++ mot1/js/style.js
@@ -1,78 +1,95 @@
 
 $(function () {
-    $('.burger').on('click', function () {
-        $('.header__menu').toggleClass('active');
-    })
+  $('.burger').on('click', function () {
+    $('.header__menu').toggleClass('active');
+  })
 
-    $('.slider').slick({
-        slidesToShow: 1,
-        fade: true,
-        waitForAnimate: true
-    });
-    $('.offer__slider').slick({
-        
-        variableWidth:true,
-        centerMode: true,
-        arrows:false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings:{
-                    slidesToShow: 2,
-                    centerMode: false,
-                    variableWidth:false,
-                    
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                  slidesToShow: 1,
-                  centerMode: false,
-                    variableWidth:false
-                }
-              }
-        ]
-    });
-    $('.slider__fly').slick({
-        slidesToShow: 2,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1
-                
-              }
-            }
-        ]
-    });
+  $('.slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    waitForAnimate: true
+  });
+  $('.offer__slider').slick({
 
-    $('.bikes__slider').slick({
-        slidesToShow: 4,
-        waitForAnimate:false,
-        responsive: [
-            {
-              breakpoint: 1366,
-              settings: {
-                slidesToShow: 3,
-                
-              }
-            },
-            {
-              breakpoint: 950,
-              settings: {
-                slidesToShow: 2,
-                
-              }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                  slidesToShow: 1,
-                  
-                }
-              }
-          ]
-    });
+    variableWidth: true,
+    centerMode: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+          variableWidth: false,
+
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+          variableWidth: false
+        }
+      }
+    ]
+  });
+  $('.slider__fly').slick({
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+
+        }
+      }
+    ]
+  });
+
+  $('.bikes__slider').slick({
+    slidesToShow: 4,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 1366,
+        settings: {
+          slidesToShow: 3,
+
+        }
+      },
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+
+        }
+      }
+    ]
+  });
+
+
+  var btnTop = $('.btn-top');
+
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 50) {
+      btnTop.fadeIn();
+    }
+    else {
+      btnTop.fadeOut();
+    }
+  });
+
+
+  btnTop.on('click', function () {
+    $('html,body').animate({ scrollTop: 0 }, 900);
+  });
 });
 
