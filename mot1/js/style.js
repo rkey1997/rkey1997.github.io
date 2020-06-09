e3f51658cefed64648477210bc2108b220162799
rkey1997 <kopytov_rodion@mail.ru>
2020-06-09 12:57:51 +0400

    mot1

---
+++ mot1/js/style.js
@@ -0,0 +1,78 @@
+
+$(function () {
+    $('.burger').on('click', function () {
+        $('.header__menu').toggleClass('active');
+    })
+
+    $('.slider').slick({
+        slidesToShow: 1,
+        fade: true,
+        waitForAnimate: true
+    });
+    $('.offer__slider').slick({
+        
+        variableWidth:true,
+        centerMode: true,
+        arrows:false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings:{
+                    slidesToShow: 2,
+                    centerMode: false,
+                    variableWidth:false,
+                    
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  centerMode: false,
+                    variableWidth:false
+                }
+              }
+        ]
+    });
+    $('.slider__fly').slick({
+        slidesToShow: 2,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+                
+              }
+            }
+        ]
+    });
+
+    $('.bikes__slider').slick({
+        slidesToShow: 4,
+        waitForAnimate:false,
+        responsive: [
+            {
+              breakpoint: 1366,
+              settings: {
+                slidesToShow: 3,
+                
+              }
+            },
+            {
+              breakpoint: 950,
+              settings: {
+                slidesToShow: 2,
+                
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  
+                }
+              }
+          ]
+    });
+});
+
